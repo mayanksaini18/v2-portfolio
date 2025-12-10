c2b24ec24e28276e037c0007de22777d96a64af1
Mayank Saini <mayanksaini0416@gmail.com>
2025-12-10 10:38:30 +0530

    unde the chnage

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" style={{ fontSize: "12px" }}>
+    <html lang="en" style={{ fontSize: "15px" }}>
       <body>{children}</body>
     </html>
   )
